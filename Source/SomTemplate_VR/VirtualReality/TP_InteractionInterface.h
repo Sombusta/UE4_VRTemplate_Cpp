6f71ad748eb8a1f3fcc2e4de22803e8cea6e022a
Som E'Bul <wjdtjr2676@gmail.com>
2018-11-14 13:48:40 +0900

    4.21 Prepare

--- Source/SomTemplate_VR/VirtualReality/TP_InteractionInterface.h
+++ Source/SomTemplate_VR/VirtualReality/TP_InteractionInterface.h
@@ -20,9 +20,9 @@ class SOMTEMPLATE_VR_API ITP_InteractionInterface
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "VRTemplate|BPI_Lowlevel|Player")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "VRTemplate|BPI_Lowlevel|Player")
 	void Pickup(class USceneComponent* AttachTo);
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "VRTemplate|BPI_Lowlevel|Player")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "VRTemplate|BPI_Lowlevel|Player")
 	void Drop();
 };
